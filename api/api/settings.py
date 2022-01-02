7ea19860abbd8d284284e1cf29cd96bd13c7dad9
jefferson-liew <liew.yew.loong.jefferson@gmail.com>
2022-01-02 15:32:25 +0800

    update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -21,7 +21,7 @@ SITE_ID = 1
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8#%9t2p&#pjzcohl9i3m$hwg2+jt)(kymlq8&dubj39+yke-ew'
+SECRET_KEY = '' #enter django security key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
